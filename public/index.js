8db429e26d7b6f56782572110014766a346083bf
Ryan Bozarth <ryan.bozarth@gmail.com>
2017-01-03 16:59:38 -0800

    added additional html pages and updated index.js with static object

--- public/index.js
+++ public/index.js
@@ -1,1 +1,51 @@
-// comment
+var MOCK_STATUS_UPDATES = {
+	{
+    "events": [
+        {
+            "id": "001",
+            "title": "North Coast Ocean Ride",
+            "date": "Jan 14, 2017",
+            "level": "beginner",
+            "attendees": "John Doe", "Jane Smith"
+        },
+        {
+            "id": "002",
+            "title": "East Bay Hills All Day",
+            "date": "Jan 21, 2017",
+            "level": "intermediate",
+            "attendees": "John Doe"
+        },
+        {
+            "id": "003",
+            "title": "Sound Coast Loop – SF to SC",
+            "date": "Jan 30, 2017",
+            "level": "advanced",
+            "attendees": "John Doe", "Jane Smith", "Jack Anderson"
+        },
+        {
+            "title": "Pacifica Surf and Breakfast",
+            "date": "Feb 7, 2017",
+            "level": "beginner",
+            "attendees": "Jane Smith"
+        }
+    ]
+};
+
+function getAllEvents(callback) {
+	 setTimeout(() => callback(MOCK_STATUS_UPDATES)}, 100);
+}
+
+function displayAllEvents(data) {
+	for (index in data.events) {
+		$('body').append(
+			`<p> ${data.events[index].text} </p>`)
+	}
+}
+
+function getAndDisplayAllEvents() {
+	getAllEvents(displayAllEvents);
+}
+
+$(function() {
+	getAndDisplayAllEvents();
+});
