cb4ab1d459b0920eb2e2abdf580cf66f369b72c4
Ryan Bozarth <ryan.bozarth@gmail.com>
2017-01-04 18:00:42 -0800

    Building out html and css for intro page

--- build/moto-event-manager.1.0.0.js
+++ build/moto-event-manager.1.0.0.js
@@ -42,30 +42,106 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	/* WEBPACK VAR INJECTION */(function(process) {'use strict';
+	
+	// Credentials //
+	app.post('/', jsonParser, function (req, res) {
+	    if (!req.body) {
+	        return res.status(400).json({
+	            message: "No request body"
+	        });
+	    }
+	
+	    if (!('username' in req.body)) {
+	        return res.status(422).json({
+	            message: 'Missing field: username'
+	        });
+	    }
+	
+	    var username = req.body.username;
+	
+	    if (typeof username !== 'string') {
+	        return res.status(422).json({
+	            message: 'Incorrect field type: username'
+	        });
+	    }
+	
+	    username = username.trim();
+	
+	    if (username === '') {
+	        return res.status(422).json({
+	            message: 'Incorrect field length: username'
+	        });
+	    }
+	
+	    if (!('password' in req.body)) {
+	        return res.status(422).json({
+	            message: 'Missing field: password'
+	        });
+	    }
+	
+	    var password = req.body.password;
+	
+	    if (typeof password !== 'string') {
+	        return res.status(422).json({
+	            message: 'Incorrect field type: password'
+	        });
+	    }
+	
+	    password = password.trim();
+	
+	    if (password === '') {
+	        return res.status(422).json({
+	            message: 'Incorrect field length: password'
+	        });
+	    }
+	
+	    var user = new User({
+	        username: username,
+	        password: password
+	    });
+	
+	    user.save(function (err) {
+	        if (err) {
+	            return res.status(500).json({
+	                message: 'Internal server error'
+	            });
+	        }
+	
+	        return res.status(201).json({});
+	    });
+	});
+	
+	mongoose.connect('mongodb://localhost/auth').then(function () {
+	    app.listen(process.env.PORT || 8080);
+	});
+	
+	// Event Handlers //
 	
 	$(function () {
-	    getAndDisplayAllEvents();
+	    // getAndDisplayAllEvents();
 	
-	    $('.btn-create-account').submit(function (e) {
+	    $('.btn-create-account').on("click", function (e) {
 	        e.preventDefault();
 	        createNewAccount();
-	        console.log(email);
-	        console.log(password);
 	    });
 	});
 	
 	function createNewAccount() {
 	    var email = $('#email-input').val();
 	    var password = $('').val();
+	    console.log(email);
+	    console.log(password);
 	    if (email === '') {
 	        alert('Please enter a valid email address.');
 	    } else if (password === '') {
 	        alert('Please enter a valid password.');
 	    }
 	}
+	
+	// Mock Events //
 	
 	var MOCK_EVENTS = {
 	    "events": [{
@@ -94,21 +170,207 @@
 	    }]
 	};
 	
-	function getAllEvents(callback) {
-	    setTimeout(function () {
-	        return callback(MOCK_EVENTS);
-	    }, 100);
-	}
+	// function getAllEvents(callback) {
+	// 	 setTimeout(() => callback(MOCK_EVENTS), 100);
+	// }
+	//
+	// function displayAllEvents(data) {
+	// 	for (index in data.events) {
+	// 		$('body').append(
+	// 			`<p> ${data.events[index].text} </p>`)
+	// 	}
+	// }
+	//
+	// function getAndDisplayAllEvents() {
+	// 	getAllEvents(displayAllEvents);
+	// }
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	// shim for using process in browser
+	var process = module.exports = {};
 	
-	function displayAllEvents(data) {
-	    for (index in data.events) {
-	        $('body').append('<p> ' + data.events[index].text + ' </p>');
+	// cached from whatever global is present so that test runners that stub it
+	// don't break things.  But we need to wrap it in a try catch in case it is
+	// wrapped in strict mode code which doesn't define any globals.  It's inside a
+	// function because try/catches deoptimize in certain engines.
+	
+	var cachedSetTimeout;
+	var cachedClearTimeout;
+	
+	function defaultSetTimout() {
+	    throw new Error('setTimeout has not been defined');
+	}
+	function defaultClearTimeout () {
+	    throw new Error('clearTimeout has not been defined');
+	}
+	(function () {
+	    try {
+	        if (typeof setTimeout === 'function') {
+	            cachedSetTimeout = setTimeout;
+	        } else {
+	            cachedSetTimeout = defaultSetTimout;
+	        }
+	    } catch (e) {
+	        cachedSetTimeout = defaultSetTimout;
+	    }
+	    try {
+	        if (typeof clearTimeout === 'function') {
+	            cachedClearTimeout = clearTimeout;
+	        } else {
+	            cachedClearTimeout = defaultClearTimeout;
+	        }
+	    } catch (e) {
+	        cachedClearTimeout = defaultClearTimeout;
+	    }
+	} ())
+	function runTimeout(fun) {
+	    if (cachedSetTimeout === setTimeout) {
+	        //normal enviroments in sane situations
+	        return setTimeout(fun, 0);
+	    }
+	    // if setTimeout wasn't available but was latter defined
+	    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+	        cachedSetTimeout = setTimeout;
+	        return setTimeout(fun, 0);
+	    }
+	    try {
+	        // when when somebody has screwed with setTimeout but no I.E. maddness
+	        return cachedSetTimeout(fun, 0);
+	    } catch(e){
+	        try {
+	            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+	            return cachedSetTimeout.call(null, fun, 0);
+	        } catch(e){
+	            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+	            return cachedSetTimeout.call(this, fun, 0);
+	        }
+	    }
+	
+	
+	}
+	function runClearTimeout(marker) {
+	    if (cachedClearTimeout === clearTimeout) {
+	        //normal enviroments in sane situations
+	        return clearTimeout(marker);
+	    }
+	    // if clearTimeout wasn't available but was latter defined
+	    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+	        cachedClearTimeout = clearTimeout;
+	        return clearTimeout(marker);
+	    }
+	    try {
+	        // when when somebody has screwed with setTimeout but no I.E. maddness
+	        return cachedClearTimeout(marker);
+	    } catch (e){
+	        try {
+	            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+	            return cachedClearTimeout.call(null, marker);
+	        } catch (e){
+	            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+	            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+	            return cachedClearTimeout.call(this, marker);
+	        }
+	    }
+	
+	
+	
+	}
+	var queue = [];
+	var draining = false;
+	var currentQueue;
+	var queueIndex = -1;
+	
+	function cleanUpNextTick() {
+	    if (!draining || !currentQueue) {
+	        return;
+	    }
+	    draining = false;
+	    if (currentQueue.length) {
+	        queue = currentQueue.concat(queue);
+	    } else {
+	        queueIndex = -1;
+	    }
+	    if (queue.length) {
+	        drainQueue();
 	    }
 	}
 	
-	function getAndDisplayAllEvents() {
-	    getAllEvents(displayAllEvents);
+	function drainQueue() {
+	    if (draining) {
+	        return;
+	    }
+	    var timeout = runTimeout(cleanUpNextTick);
+	    draining = true;
+	
+	    var len = queue.length;
+	    while(len) {
+	        currentQueue = queue;
+	        queue = [];
+	        while (++queueIndex < len) {
+	            if (currentQueue) {
+	                currentQueue[queueIndex].run();
+	            }
+	        }
+	        queueIndex = -1;
+	        len = queue.length;
+	    }
+	    currentQueue = null;
+	    draining = false;
+	    runClearTimeout(timeout);
 	}
+	
+	process.nextTick = function (fun) {
+	    var args = new Array(arguments.length - 1);
+	    if (arguments.length > 1) {
+	        for (var i = 1; i < arguments.length; i++) {
+	            args[i - 1] = arguments[i];
+	        }
+	    }
+	    queue.push(new Item(fun, args));
+	    if (queue.length === 1 && !draining) {
+	        runTimeout(drainQueue);
+	    }
+	};
+	
+	// v8 likes predictible objects
+	function Item(fun, array) {
+	    this.fun = fun;
+	    this.array = array;
+	}
+	Item.prototype.run = function () {
+	    this.fun.apply(null, this.array);
+	};
+	process.title = 'browser';
+	process.browser = true;
+	process.env = {};
+	process.argv = [];
+	process.version = ''; // empty string to avoid regexp issues
+	process.versions = {};
+	
+	function noop() {}
+	
+	process.on = noop;
+	process.addListener = noop;
+	process.once = noop;
+	process.off = noop;
+	process.removeListener = noop;
+	process.removeAllListeners = noop;
+	process.emit = noop;
+	
+	process.binding = function (name) {
+	    throw new Error('process.binding is not supported');
+	};
+	
+	process.cwd = function () { return '/' };
+	process.chdir = function (dir) {
+	    throw new Error('process.chdir is not supported');
+	};
+	process.umask = function() { return 0; };
+
 
 /***/ }
 /******/ ]);
--- public/index.js
+++ public/index.js
@@ -1,23 +1,104 @@
-$(function() {
-	getAndDisplayAllEvents();
 
-    $('.btn-create-account').submit(function(e) {
+
+// Credentials //
+app.post('/', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.status(400).json({
+            message: "No request body"
+        });
+    }
+
+    if (!('username' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: username'
+        });
+    }
+
+    var username = req.body.username;
+
+    if (typeof username !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: username'
+        });
+    }
+
+    username = username.trim();
+
+    if (username === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: username'
+        });
+    }
+
+    if (!('password' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: password'
+        });
+    }
+
+    var password = req.body.password;
+
+    if (typeof password !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: password'
+        });
+    }
+
+    password = password.trim();
+
+    if (password === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: password'
+        });
+    }
+
+    var user = new User({
+        username: username,
+        password: password
+    });
+
+    user.save(function(err) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+
+        return res.status(201).json({});
+    });
+});
+
+mongoose.connect('mongodb://localhost/auth').then(function() {
+    app.listen(process.env.PORT || 8080);
+});
+
+
+
+
+// Event Handlers //
+
+$(function() {
+	// getAndDisplayAllEvents();
+
+    $('.btn-create-account').on( "click", function(e) {
     		e.preventDefault();
     		createNewAccount();
-            console.log(email);
-            console.log(password);
     	});
 });
 
 function createNewAccount() {
     var email = $('#email-input').val();
     var password = $('').val();
+    console.log(email);
+    console.log(password);
     if (email === '') {
         alert('Please enter a valid email address.');
     } else if (password === '') {
         alert('Please enter a valid password.');
     }
 }
+
+// Mock Events //
 
 var MOCK_EVENTS = {
     "events": [
@@ -51,17 +132,17 @@ var MOCK_EVENTS = {
     ]
 };
 
-function getAllEvents(callback) {
-	 setTimeout(() => callback(MOCK_EVENTS), 100);
-}
-
-function displayAllEvents(data) {
-	for (index in data.events) {
-		$('body').append(
-			`<p> ${data.events[index].text} </p>`)
-	}
-}
-
-function getAndDisplayAllEvents() {
-	getAllEvents(displayAllEvents);
-}
+// function getAllEvents(callback) {
+// 	 setTimeout(() => callback(MOCK_EVENTS), 100);
+// }
+//
+// function displayAllEvents(data) {
+// 	for (index in data.events) {
+// 		$('body').append(
+// 			`<p> ${data.events[index].text} </p>`)
+// 	}
+// }
+//
+// function getAndDisplayAllEvents() {
+// 	getAllEvents(displayAllEvents);
+// }
