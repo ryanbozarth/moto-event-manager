433cb3a8f36d61d3d4c4e36357134fd0354f56ba
Ryan Bozarth <ryan.bozarth@gmail.com>
2017-01-20 10:51:18 -0800

    added static event data to database

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(flash());
 
 //Express Session
 app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
+   secret: 'secret',
+   saveUninitialized: true,
+   resave: true
 }));
 
 // Passport initializ
@@ -33,36 +33,36 @@ app.use(passport.session());
 
 // Express Validator
 app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-      var namespace = param.split('.')
-      , root    = namespace.shift()
-      , formParam = root;
+   errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+         root = namespace.shift(),
+         formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
+      while (namespace.length) {
+         formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+         param: formParam,
+         msg: msg,
+         value: value
+      };
+   }
 }));
 
 // Flash messages
-app.use(function(req, res , next) {
-  res.locals.sucess_msg = req.flash('sucess_msg');
-  res.locals.erro_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
+app.use(function(req, res, next) {
+   res.locals.sucess_msg = req.flash('sucess_msg');
+   res.locals.erro_msg = req.flash('error_msg');
+   res.locals.error = req.flash('error');
+   next();
 });
 
 // Set database port
 const DATABASE_URL = process.env.DATABASE_URL ||
-  global.DATABASE_URL ||
-  (process.env.NODE_ENV === 'production' ?
-    'mongodb://ryanbozarth:QmdrKcFz3UB,zG@ds145848.mlab.com:45848/moto-event-manager' :
-    'mongodb://localhost/moto-event-manager');
+   global.DATABASE_URL ||
+   (process.env.NODE_ENV === 'production' ?
+      'mongodb://ryanbozarth:QmdrKcFz3UB,zG@ds145848.mlab.com:45848/moto-event-manager' :
+      'mongodb://localhost/moto-event-manager');
 const PORT = process.env.PORT || 8080;
 
 console.log('database_url: ' + DATABASE_URL);
@@ -70,139 +70,128 @@ console.log('port: ' + PORT);
 
 // Run and close the server
 function runServer() {
-  return new Promise((resolve, reject) => {
-    console.log(DATABASE_URL);
-    mongoose.connect(DATABASE_URL, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(PORT, () => {
-          console.log(`Your app is listening on port ${PORT}`);
+   return new Promise((resolve, reject) => {
+      console.log(DATABASE_URL);
+      mongoose.connect(DATABASE_URL, err => {
+         if (err) {
+            return reject(err);
+         }
+         server = app.listen(PORT, () => {
+               console.log(`Your app is listening on port ${PORT}`);
 
-          resolve();
-        })
-        .on('error', err => {
-          mongoose.disconnect();
-          reject(err);
-        });
-    });
-  });
+               resolve();
+            })
+            .on('error', err => {
+               mongoose.disconnect();
+               reject(err);
+            });
+      });
+   });
 }
 
 function closeServer() {
-  return mongoose.disconnect().then(() => {
-    return new Promise((resolve, reject) => {
-      console.log('Closing server');
-      server.close(err => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
+   return mongoose.disconnect().then(() => {
+      return new Promise((resolve, reject) => {
+         console.log('Closing server');
+         server.close(err => {
+            if (err) {
+               return reject(err);
+            }
+            resolve();
+         });
       });
-    });
-  });
+   });
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+   runServer().catch(err => console.error(err));
 };
 
 app.get('/', (req, res) => {
-  res.status(200);
-  res.sendFile(__dirname + '/build/index.html');
+   res.status(200);
+   res.sendFile(__dirname + '/build/index.html');
 });
 
 app.post('/register', (req, res) => {
-  var email = req.body.email;
-  var password = req.body.password;
-  console.log(email);
+   var email = req.body.email;
+   var password = req.body.password;
+   console.log(email);
 
-  bcrypt.genSalt(10, function(err, salt) {
-    if (err) {
-      console.log('there are gensalt errors');
-      return res.status(500).json({
-        message: 'Internal server error'
-      });
-    }
-    bcrypt.hash(password, salt, function(err, hash) {
+   bcrypt.genSalt(10, function(err, salt) {
       if (err) {
-        console.log(err);
-        return res.status(500).json({
-          message: 'Internal server error'
-        });
-      }
-      var user = new User({
-        email: email,
-        password: hash
-      });
-      user.save(function(err) {
-        if (err) {
-          console.log('there is an error', err);
-          return res.status(500).json({
+         console.log('there are gensalt errors');
+         return res.status(500).json({
             message: 'Internal server error'
-          });
-        }
-        return res.status(201).json(user);
+         });
+      }
+      bcrypt.hash(password, salt, function(err, hash) {
+         if (err) {
+            console.log(err);
+            return res.status(500).json({
+               message: 'Internal server error'
+            });
+         }
+         var user = new User({
+            email: email,
+            password: hash
+         });
+         user.save(function(err) {
+            if (err) {
+               console.log('there is an error', err);
+               return res.status(500).json({
+                  message: 'Internal server error'
+               });
+            }
+            return res.status(201).json(user);
+         });
       });
-    });
-  });
+   });
 });
 
 app.put('/profile/:email', (req, res) => {
-  console.log(req.body);
-    User.findOneAndUpdate({email: req.body.email}, req.body, {}, function(err, profile) {
+   User.findOneAndUpdate({
+      email: req.body.email
+   }, req.body, {}, function(err, profile) {
       if (err) {
-        return res.status(500).json({
-          message: 'Internal server error - update'
-        });
+         return res.status(500).json({
+            message: 'Internal server error - update'
+         });
       };
       return res.status(200).json(req.body);
-    });
+   });
 });
 
 app.post('/login', passport.authenticate('local'), function(req, res) {
-  if (err) {
-    throw err;
-  }
-  res.status(200);
-  res.json(user); //don't send password
-
-  // If this function gets called, authentication was successful.
-  // `req.user` contains the authenticated user.
+   if (err) {
+      throw err;
+   }
+   res.status(200);
+   res.json(user); //don't send password
 });
 
 app.get('/main/:user', (req, res) => {
-  res.sendfile('./build/main.html');
-  res.status(200);
+   res.sendfile('./build/main.html');
+   res.status(200);
 });
 
-app.get('/event-details/:id', (req, res) => {
-  res.sendfile('./build/event-details.html');
-  res.status(200);
-  // req.params.user = ryanbozarth
-  // get all rides for user
-  // database requst find by user name. get back all rides for which user name = ryanbozarth
-  // json object parse and display
-});
+app.get('/events/:id', (req, res) => {
+   Event.findOne({"id": req.params.id}, function(err, eventDetails) {
+      // '"'+req.params.id+'"'
+            console.log(req.params.id);
+            console.log(eventDetails);
 
-// app.put('/event/:id', (req, res) => {
-
-
-// req.body (has a body otherwise error)
-//   let updatedItem = Event.update({
-//     id: req.params.id;
-//     attendees: req.body.attendees;
-//   });
-//   res.status(204).json(updatedItem);
-// });
-
-app.get('/main/:id', (req, res) => {
-  res.status(200);
-});
+            if (err) {
+               return res.status(500).json({
+                  message: 'Internal server error - update'
+               });
+            };
+            return res.status(200).json(eventDetails);
+         });
+   });
 
 
 app.get("*", function(req, res) {
-  res.redirect("/");
+   res.redirect("/");
 });
 
 
@@ -211,7 +200,7 @@ exports.app = app;
 
 
 module.exports = {
-  app,
-  runServer,
-  closeServer
+   app,
+   runServer,
+   closeServer
 };
